3a4c6b100ec7253eb7d1a8fdab3b68c50197d4ce
Harindue <harindu.e@asyncmetric.com>
2024-03-20 21:40:18 +0530

    hero & showcase animations

---
+++ src/slices/Showcase/AnimatedContent.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function AnimatedContent({ children }: { children: ReactNode }) {
+  const container = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(
+    () => {
+      if (prefersReducedMotion) {
+        gsap.set(container.current, { y: 0 });
+        return;
+      }
+
+      gsap.fromTo(
+        container.current,
+        { y: 100 },
+        {
+          y: 0,
+          ease: "power2.inOut",
+          duration: 1,
+          scrollTrigger: {
+            trigger: container.current,
+            start: "top bottom-=40%",
+            toggleActions: "play pause resume reverse",
+          },
+        },
+      );
+    },
+    { scope: container },
+  );
+
+  return <div ref={container}>{children}</div>;
+}
